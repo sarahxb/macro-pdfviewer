a4c57fb45eacb64bd470487b14a69a9caa1b32f7
sarahxb <sarahbuligamarga@gmail.com>
2025-12-09 09:40:21 +0200

    Write automated tests for PDF viewer macro #85 *test for multiple pdfs *test for pdfs with multiple parameters *added pdfs for resources

--- macro-pdfviewer-test/macro-pdfviewer-test-docker/src/test/it/com/xwiki/pdfviewer/test/ui/PDFViewerIT.java
+++ macro-pdfviewer-test/macro-pdfviewer-test-docker/src/test/it/com/xwiki/pdfviewer/test/ui/PDFViewerIT.java
@@ -19,7 +19,12 @@
  */
 package com.xwiki.pdfviewer.test.ui;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Order;
@@ -33,32 +38,18 @@ import org.xwiki.test.docker.junit5.UITest;
 import org.xwiki.test.ui.TestUtils;
 import org.xwiki.test.ui.po.ViewPage;
 
-@UITest(
-    extensionOverrides = {
-        @ExtensionOverride(
-            extensionId = "com.google.code.findbugs:jsr305",
-            overrides = {
-                "features=com.google.code.findbugs:annotations"
-            }),
-        @ExtensionOverride(
-            extensionId = "org.bouncycastle:bcprov-jdk18on",
-            overrides = {
-                "features=org.bouncycastle:bcprov-jdk15on"
-            }
-        ),
-        @ExtensionOverride(
-            extensionId = "org.bouncycastle:bcpkix-jdk18on",
-            overrides = {
-                "features=org.bouncycastle:bcpkix-jdk15on"
-            }
-        ),
-        @ExtensionOverride(
-            extensionId = "org.bouncycastle:bcmail-jdk18on",
-            overrides = {
-                "features=org.bouncycastle:bcmail-jdk15on"
-            }
-        )
-    })
+import com.xwiki.pdfviewer.po.PDFViewerMacroPage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@UITest(extensionOverrides = { @ExtensionOverride(extensionId = "com.google.code.findbugs:jsr305", overrides = {
+    "features=com.google.code.findbugs:annotations" }),
+    @ExtensionOverride(extensionId = "org.bouncycastle:bcprov-jdk18on", overrides = {
+        "features=org.bouncycastle:bcprov-jdk15on" }),
+    @ExtensionOverride(extensionId = "org.bouncycastle:bcpkix-jdk18on", overrides = {
+        "features=org.bouncycastle:bcpkix-jdk15on" }),
+    @ExtensionOverride(extensionId = "org.bouncycastle:bcmail-jdk18on", overrides = {
+        "features=org.bouncycastle:bcmail-jdk15on" }) })
 public class PDFViewerIT
 {
     @BeforeAll
@@ -69,11 +60,34 @@ public class PDFViewerIT
 
     @Test
     @Order(1)
-    void createPDF(TestUtils setup,TestConfiguration testConfiguration)
+    void createPDF(TestUtils setup, TestConfiguration testConfiguration)
     {
-        createPage( setup,"{{pdfviewer file=\"PDFTest.pdf\"/}}", "PageWithUploadedPDF");
-        uploadFile("PDFTest.pdf",testConfiguration);
+        createPage(setup, getMacroContent("pdfMacroContent.vm"), "PageWithUploadedPDF");
 
+        uploadFile("PDFTest.pdf", testConfiguration);
+        PDFViewerMacroPage page = new PDFViewerMacroPage();
+        page.reloadPage();
+
+        //setup.getDriver().waitUntilCondition(driver->(page.getPDFViewerMacrosCount())==4);
+        assertEquals(4, page.getPDFViewerMacrosCount(), 60000);
+        setup.getDriver().waitUntilPageIsReloaded();
+//        PDFViewerMacro viewer0 = page.getPDFViewer(0);
+//        PDFViewerMacro viewer1 = page.getPDFViewer(1);
+//        PDFViewerMacro viewer2 = page.getPDFViewer(2);
+//        PDFViewerMacro viewer3 = page.getPDFViewer(3);
+
+    }
+
+    @Test
+    @Order(2)
+    void createPageWithMultiplePDFs(TestUtils setup, TestConfiguration testConfiguration)
+    {
+        createPage(setup, getMacroContent("multiplePDFs.vm"), "PageWithMultiplePDFs");
+
+        uploadFile("PDFTest.pdf", testConfiguration);
+        uploadFile("PDFTest-1.pdf", testConfiguration);
+        uploadFile("PDFTest-2.pdf", testConfiguration);
+        uploadFile("PDFTest-3.pdf", testConfiguration);
     }
 
     private void uploadFile(String attachmentName, TestConfiguration testConfiguration)
@@ -84,10 +98,24 @@ public class PDFViewerIT
         sourceAttachmentsPane.setFileToUpload(attachmentPath);
         sourceAttachmentsPane.waitForUploadToFinish(attachmentName);
     }
+
     private ViewPage createPage(TestUtils setup, String content, String pageName)
     {
         DocumentReference documentReference = new DocumentReference("xwiki", "PDFViewerMacro", pageName);
         return setup.createPage(documentReference, content);
     }
 
+    private String getMacroContent(String filename)
+    {
+        try (InputStream inputStream = getClass().getResourceAsStream("/pdfmacro/" + filename)) {
+            if (inputStream == null) {
+                throw new RuntimeException("Failed to load " + filename + " from resources.");
+            }
+
+            return new BufferedReader(new InputStreamReader(inputStream)).lines()
+                .filter(line -> !line.trim().startsWith("##")).collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read macro file: " + filename, e);
+        }
+    }
 }
